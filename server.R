c9d0e4e8f0d7aaf35b68a14669988b3811384f93
Eduard <eduardsan@gmail.com>
2018-03-01 19:04:38 +0100

    Initial working version

--- server.R
+++ server.R
@@ -1,26 +1,61 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
   output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+    # draw the density plot with the specified parameters
+    normdata <-
+      as.data.frame(x = rnorm(input$num, mean = input$mean, sd = input$sd))
+    names(normdata) <- c("values")
+    pos <-
+      ifelse(
+        input$radio == 1,
+        input$param,
+        qnorm(
+          input$param,
+          mean = input$mean,
+          sd = input$sd,
+          lower.tail = input$lower.tail
+        )
+      )
+    ggplot(data = normdata, aes(values)) +
+      geom_density(aes(color = "density")) +
+      geom_vline(aes(xintercept = input$mean, color = "mean")) +
+      geom_vline(aes(xintercept = pos, color = "quantile")) +
+      scale_color_manual(name = "", values = c(density = "blue", mean = "forestgreen", quantile = "firebrick"))
   })
-  
+  output$svrq <- renderText({
+    ifelse(
+      input$radio == 1,
+      input$param,
+      qnorm(
+        input$param,
+        mean = input$mean,
+        sd = input$sd,
+        lower.tail = input$lower.tail
+      )
+    )
+  })
+  output$svrp <- renderText({
+    ifelse(
+      input$radio == 1,
+      pnorm(
+        input$param,
+        mean = input$mean,
+        sd = input$sd,
+        lower.tail = input$lower.tail
+      ),
+      input$param
+    )
+  })
 })
--- ui.R
+++ ui.R
@@ -13,21 +13,49 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Normal Distribution Sandbox"),
+  
+  wellPanel(helpText("This Shiny website lets you experiment with a number of parameters related to ",
+                     "a normal distribution. Use the controls on the left panel to specify the mean ",
+                     "of the normal distribution, its standard deviation and the number of randomly ", 
+                     "generated points that will be used to generate the density plot.",
+                     "Additionally, you can provide a numeric input (quantile or probability) and set the
+                     lower tail parameter. These values will be used to compute the corresponding quantile
+                     and probability values that are displayed below the plot.")),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
+       sliderInput("mean",
+                   "Mean of the normal distribution:",
+                   min = -50,
                    max = 50,
-                   value = 30)
+                   value = 0),
+       sliderInput("sd",
+                   "Standard deviation:",
+                   min = 1,
+                   max = 20,
+                   value = 1),
+       numericInput("num", label = "Number of points:", value = 100),
+       radioButtons("radio", label = "Value type:",
+                    choices = list("Quantile" = 1, "Probability" = 2), 
+                    selected = 2),
+       numericInput("param", label = "Quantile/probability value:", value = 0.95),
+       checkboxInput("lower.tail", label = "Lower tail", value = TRUE),
+       submitButton("Submit")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("distPlot"),
+       hr(),
+       fluidRow(
+         column(width = 3, h3("Quantile")), 
+         column(width = 3, offset = 2, h3("Probability"))
+       ),
+       fluidRow(
+         column(3, verbatimTextOutput("svrq")),
+         column(width = 3, offset = 2, verbatimTextOutput("svrp")))
     )
   )
 ))
