aaba93d4507012523e72073a8cfe3a2e20511e7b
Eduard <eduardsan@gmail.com>
2018-03-01 19:49:38 +0100

    First complete version

--- server.R
+++ server.R
@@ -1,6 +1,5 @@
 #
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
+# This is the server logic of a Shiny web application.
 #
 # Find out more about building applications with Shiny here:
 #
@@ -10,30 +9,40 @@
 library(shiny)
 library(ggplot2)
 
-# Define server logic required to draw a histogram
+# Define server logic
 shinyServer(function(input, output) {
+  
+  # draw plot
   output$distPlot <- renderPlot({
-    # draw the density plot with the specified parameters
+    # generate random numbers following a normal distribution with the given paramenters
     normdata <-
       as.data.frame(x = rnorm(input$num, mean = input$mean, sd = input$sd))
-    names(normdata) <- c("values")
+    names(normdata) <- c("x")
+
+    # draw reference density plot and density plot with random numbers
+    p <- ggplot(data = normdata, aes(x)) + 
+      stat_function(fun = dnorm, n = 1000, args = list(mean = input$mean, sd = input$sd), 
+                    aes(colour = "normal")) +
+      scale_y_continuous(breaks = NULL) + 
+      geom_density(aes(color = "random"))
+
+    # determine where to draw the quantile line
     pos <-
       ifelse(
         input$radio == 1,
         input$param,
-        qnorm(
-          input$param,
-          mean = input$mean,
-          sd = input$sd,
-          lower.tail = input$lower.tail
-        )
+        qnorm(input$param, mean = input$mean, sd = input$sd, lower.tail = input$lower.tail)
       )
-    ggplot(data = normdata, aes(values)) +
-      geom_density(aes(color = "density")) +
-      geom_vline(aes(xintercept = input$mean, color = "mean")) +
+    
+    # add mean and quantile lines to plot
+    p <- p + geom_vline(aes(xintercept = input$mean, color = "mean")) +
       geom_vline(aes(xintercept = pos, color = "quantile")) +
-      scale_color_manual(name = "", values = c(density = "blue", mean = "forestgreen", quantile = "firebrick"))
+      scale_color_manual(
+        name = "", 
+        values = c(normal = "dimgray", random = "blue", mean = "forestgreen", quantile = "firebrick"))
+    p
   })
+  # compute quantile value
   output$svrq <- renderText({
     ifelse(
       input$radio == 1,
@@ -46,6 +55,7 @@ shinyServer(function(input, output) {
       )
     )
   })
+  # compute probability value
   output$svrp <- renderText({
     ifelse(
       input$radio == 1,
--- ui.R
+++ ui.R
@@ -1,6 +1,5 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# This is the user-interface definition of a Shiny web application.
 #
 # Find out more about building applications with Shiny here:
 # 
@@ -9,21 +8,26 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Normal Distribution Sandbox"),
   
+  # Help text
   wellPanel(helpText("This Shiny website lets you experiment with a number of parameters related to ",
                      "a normal distribution. Use the controls on the left panel to specify the mean ",
                      "of the normal distribution, its standard deviation and the number of randomly ", 
-                     "generated points that will be used to generate the density plot.",
+                     "generated points that will be used to generate a density plot.",
                      "Additionally, you can provide a numeric input (quantile or probability) and set the
                      lower tail parameter. These values will be used to compute the corresponding quantile
-                     and probability values that are displayed below the plot.")),
+                     and probability values that are displayed below the plot. Press the submit button
+                     to send the specified parameters and update the plot."),
+            helpText("The plot shows a \"perfect\" normal for reference (grey curve), a density plot",
+                     "based on the randomly generated points (blue curve), the given mean (green line)",
+                     "and the position of the given/computed quantile (red line).")),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with input controls
   sidebarLayout(
     sidebarPanel(
        sliderInput("mean",
@@ -36,7 +40,7 @@ shinyUI(fluidPage(
                    min = 1,
                    max = 20,
                    value = 1),
-       numericInput("num", label = "Number of points:", value = 100),
+       numericInput("num", label = "Number of normally distributed random points:", value = 100),
        radioButtons("radio", label = "Value type:",
                     choices = list("Quantile" = 1, "Probability" = 2), 
                     selected = 2),
